8815042107b121d63b37177557a4a3cefeb4a28e
fedosovmu <fedosovmu@gmail.com>
2020-12-19 18:21:06 +0300

    Удалена тестовая кнопка. Теперь элементы списка ведут себя по разному в зависимости от длительности нажатия

--- main_screen.py
+++ main_screen.py
@@ -16,16 +16,6 @@ class MainScreen(Screen):
         # Создание выпадающего меню
         self.menu = MDDropdownMenu()
 
-    def test_button_on_press(self):
-        self.ids.test_button.last_press_time = datetime.now()
-        print('test button press')
-
-    def test_button_on_release(self):
-        release_time = datetime.now()
-        delay = release_time - self.ids.test_button.last_press_time
-        delay_in_seconds = round(delay.total_seconds(), 2)
-        print('test button on release {}'.format(delay_in_seconds))
-
     def load_notes(self):
         notes = self.app.db_handler.select_notes()
         for note in notes:
@@ -37,14 +27,29 @@ class MainScreen(Screen):
         db_id = self.app.db_handler.insert_note(title)
         self.add_note_to_screen(db_id=db_id, title=title)
 
+    def list_item_on_press_callback(self, list_item):
+        list_item.last_press_time = datetime.now()
+
+    def list_item_on_release_callback(self, list_item):
+        release_time = datetime.now()
+        delay = release_time - list_item.last_press_time
+        delay_in_seconds = delay.total_seconds()
+        LONG_PRESS_TIME = 0.5
+        if delay_in_seconds < LONG_PRESS_TIME:
+            # short press
+            self.app.go_to_note_screen('db_id: {}'.format(list_item.db_id))
+        else:
+            # long press
+            print('long_press')
+
     def add_note_to_screen(self, db_id, title):
         new_note = OneLineListItem(text=title)
         new_note.db_id = db_id
         new_note.bind(
-            on_press=lambda x: self.app.go_to_note_screen('db_id: {}'.format(new_note.db_id))
+            on_press=lambda list_item: self.list_item_on_press_callback(list_item)
         )
         new_note.bind(
-            on_long_press=lambda x: print('on_release')
+            on_release=lambda list_item: self.list_item_on_release_callback(list_item)
         )
         self.ids.notes_list.add_widget(
                 new_note
