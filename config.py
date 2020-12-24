5fa6b692363339a1271cd64b0b8222c4c2c1b742
fedosovmu <fedosovmu@gmail.com>
2020-12-24 16:37:55 +0300

    Update requirements

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-import kivy
-kivy.require('2.0.0')
+from kivy import require
 from kivy.config import Config
+
+
+require('2.0.0')
 Config.set('graphics', 'width', '360')
 Config.set('graphics', 'height', '640')
